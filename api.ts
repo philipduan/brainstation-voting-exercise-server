0b919854c9eb14660bb11b87ab2890884a5f3b3d
Philip Du <philip_du_an@hotmail.com>
2023-06-06 19:12:10 -0700

    Reset cors

--- api.ts
+++ api.ts
@@ -7,19 +7,13 @@ import "./db/mongoose";
 import { appRouter } from "./routers";
 import { createContext } from "./context";
 const app = express();
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  // Pass to next layer of middleware
-  next();
-});
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL as string,
+    credentials: true,
+    preflightContinue: true,
+  })
+);
 
 app.use(
   "/trpc",
